81112c4ab3bf406e15eefb9def90cef75ae65a64
Akshay Kumar <akshay.kumar@lytx.com>
2024-12-11 10:29:53 +0530

    CrudOnUserMgt

--- DemoApp/src/app/app.module.ts
+++ DemoApp/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { FormsModule } from '@angular/forms';
 import { DemoService } from './services/demo.service';
 import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormComponent } from './form/form.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, FormComponent],
+  declarations: [AppComponent, FirstComponent, FormComponent, UsersComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule ],
   providers: [DemoService, provideHttpClient(withInterceptorsFromDi())],
   bootstrap: [AppComponent],
--- DemoApp/src/app/services/demo.service.ts
+++ DemoApp/src/app/services/demo.service.ts
@@ -1,27 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { IUser } from '../IUser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DemoService{
 
-  listOfUser = [
-    { name: 'John', isActive: true },
-    { name: 'Joe', isActive: true },
-    { name: 'Mike', isActive: false },
-    { name: 'Essan', isActive: true },
-  ];
-  constructor(private httpClient : HttpClient) { }
-
-
-  getValuesFromApi() : Observable<any> {
-     return this.httpClient.get('https://localhost:7122/api/User')
+  constructor(private httpClient: HttpClient) { }
+  getValues(): string[] {
+    throw new Error('Method not implemented.');
   }
-
-
-  getValues(): Observable<any> {
-    return of(this.listOfUser)
+  getValuesFromAPI():Observable<any>{
+    return this.httpClient.get('https://localhost:7122/api/User');
+   }
+   createUser(user:IUser): Observable<IUser> {
+    return this.httpClient.post<IUser>('https://localhost:7122/api/User', user);
+  }
+  deleteUser(id: number): Observable<boolean> {
+    return this.httpClient.delete<boolean>(`https://localhost:7122/api/User/${id}`); // Send DELETE request to API
+  }
+  updateUser(user: IUser): Observable<any> {
+    return this.httpClient.put(`https://localhost:7122/api/User/${user.id}`, user); // HTTP PUT request to update the user
   }
 }
